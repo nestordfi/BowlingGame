519d6298acad38343e14f2071d587c1bcb265328
Nestor Franco <nestor.franco@ndeveloper.com>
2021-10-18 12:55:43 -0500

    TDD implementando se realiza todas las prueba de las variantes del juegi

--- src/main/java/ec/nestordfi/controller/BowlingGame.java
+++ src/main/java/ec/nestordfi/controller/BowlingGame.java
@@ -1,10 +1,38 @@
 package ec.nestordfi.controller;
 
 public class BowlingGame {
+	
+	private int[] rolls = new int[21];
+	private int currentRoll = 0;
 
-	public void roll() {
-		// TODO Auto-generated method stub
-		
+	public void roll(int pins) {
+		rolls[currentRoll++] = pins;
+	}
+
+	public Object score() {
+		int score = 0;
+		int frameIndex = 0;
+		for (int frame = 0; frame < 10; frame++) {
+			if (isStrike(frameIndex)) {
+				score += 10 + rolls[frameIndex+1] + rolls[frameIndex+2];
+				frameIndex++;
+			}else if(isSpare(frameIndex)) {
+				score += 10 + rolls[frameIndex+2];
+				frameIndex += 2;
+			}else {
+				score += rolls[frameIndex] + rolls[frameIndex+1];
+				frameIndex += 2;				
+			}
+		}
+		return score;
+	}
+
+	private boolean isStrike(int frameIndex) {
+		return rolls[frameIndex] == 10;
+	}
+
+	private boolean isSpare(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex+1] == 10;
 	}
 
 }
--- src/test/java/ec/nestordfi/test/BowlingGameTest.java
+++ src/test/java/ec/nestordfi/test/BowlingGameTest.java
@@ -1,19 +1,67 @@
 package ec.nestordfi.test;
 
-import java.util.Iterator;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import ec.nestordfi.controller.BowlingGame;
 
 public class BowlingGameTest {
 	
+	private BowlingGame bowlingGame;
+	
+	@Before
+	public void SetUp() throws Exception{
+		bowlingGame = new BowlingGame();
+	}
+	
 	@Test
 	public void testGutterGame() {
-		BowlingGame bowlingGame = new BowlingGame();
-		for (int i = 0; i < 20; i++) {
-			bowlingGame.roll();
+		rollMany(20, 0);
+		assertEquals(0, bowlingGame.score());
+	}
+	
+	@Test
+	public void testAllOnes() {
+		rollMany(20, 1);
+		assertEquals(20, bowlingGame.score());
+	}
+	
+	@Test
+	public void testOneSpare() {
+		rollSpare();
+		bowlingGame.roll(3);
+		rollMany(17, 0);
+		assertEquals(16, bowlingGame.score());
+	}
+	
+	@Test
+	public void testOneStrike() {
+		bowlingGame.roll(10);
+		bowlingGame.roll(3);
+		bowlingGame.roll(4);
+		rollMany(16, 0);
+		assertEquals(24, bowlingGame.score());
+	}
+	
+	@Test
+	public void testPerfectGame() {
+		rollMany(12, 10);
+		assertEquals(300, bowlingGame.score());
+	}
+
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++) {
+			bowlingGame.roll(pins);
 		}
 	}
+	
+	private void rollSpare() {
+		bowlingGame.roll(5);
+		bowlingGame.roll(5);
+	}
+	
+	
 
 }
